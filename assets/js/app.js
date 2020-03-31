a872a226da95fdf8699301fffcc3a78f53a8ea59
aprivalov <a.privalov33@gmail.com>
2020-03-31 17:56:20 +0200

    Addet Front, Back controllers + templates. Edited connection BD in to .env

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,4 +13,3 @@ require('bootstrap');
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
